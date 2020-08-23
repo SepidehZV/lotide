78576f597a5e4a9f825d2fef610392adccc61fdd
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-08-23 21:08:21 +0000

    refacore eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertation Failed: ${actual} !== ${expected}`);
-  }
-};
+//eqArrays function definition
 const eqArrays = function(arr1,arr2) {
   for (let i = 0; i < Math.max(arr1.length,arr2.length); ++i) {
     if (arr1[i] !== arr2[i]) {
@@ -13,8 +7,5 @@ const eqArrays = function(arr1,arr2) {
   }
   return true;
 };
+module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
