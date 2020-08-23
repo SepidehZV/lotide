149c9bda299397b6cacd83b3ceb4895ded4421e5
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-08-23 22:17:44 +0000

    incorporate mocha & chai in tailTest and middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
-// TEST CODE
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
-assertArraysEqual(middle([2]),[]);
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1,2]),[]);
+
+describe("#middle", () => {
+  it("returns [3] for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+  it("returns [] for [2]", () => {
+    assert.deepEqual(middle([2]), []);
+  });
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+  it("returns [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+});
